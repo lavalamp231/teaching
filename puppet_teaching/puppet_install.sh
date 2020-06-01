5a79800f319f356d75109884056cacdd3adf8cd2
lavalamp231 <andrew.w.simons@gmail.com>
2020-06-01 13:32:58 -0500

    fixing stuff

--- puppet_teaching/puppet_install.sh
+++ puppet_teaching/puppet_install.sh
@@ -6,13 +6,9 @@ rpm -Uvh https://yum.puppet.com/puppet6-release-el-7.noarch.rpm
 
 yum install puppet -y
 
-exec bash
-
-"echo populating"
+echo "populating puppet.conf"
 
 echo "[main]
 server = puppet-server.exercise.com
 environment = production
 runinterval = 3600" > /etc/puppetlabs/puppet/puppet.conf
-
-reboot
