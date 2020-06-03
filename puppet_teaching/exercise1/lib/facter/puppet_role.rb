87a47526abd5c406682bf76ff710f432e6a5a2c6
lavalamp231 <andrew.w.simons@gmail.com>
2020-06-02 22:55:33 -0500

    updating fact

--- puppet_teaching/exercise1/lib/facter/puppet_role.rb
+++ puppet_teaching/exercise1/lib/facter/puppet_role.rb
@@ -1,5 +1,5 @@
 Facter.add(:puppet_role) do
   setcode do
-    Facter::Core::Execution.execute("/usr/bin/grep Puppet_Role= /etc/puppet_role.txt | /usr/bin/sed 's/Puppet_Role=//'")
+    Facter::Core::Execution.execute("/usr/bin/grep -i puppet_role= /etc/puppet_role.txt | /usr/bin/sed 's/Puppet_Role=//'")
   end
 end
